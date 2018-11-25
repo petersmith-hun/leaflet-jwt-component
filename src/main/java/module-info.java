b15dff5ce7aaf547aa01f7033647c0fff4825ac2
Péter Kovács <peter.kovacs0519@gmail.com>
2018-11-25 15:23:51 +0100

    LFLT-307 Migrate common Leaflet components to OpenJDK 11  - Added module-info.java  - Fixed Surefire issue

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module leaflet.jwt.component {
+    requires java.sql;
+    requires java.validation;
+    requires javax.servlet.api;
+    requires h2;
+    requires jackson.databind;
+    requires jjwt;
+    requires org.apache.commons.lang3;
+    requires slf4j.api;
+    requires spring.beans;
+    requires spring.boot;
+    requires spring.context;
+    requires spring.core;
+    requires spring.jdbc;
+    requires spring.security.core;
+    requires spring.security.web;
+    requires spring.tx;
+}
